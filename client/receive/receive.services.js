b2eaf046b284394490b0f7e6389ec74d5dd10767
Etienne Tripier <etripier@gmail.com>
2014-09-17 11:51:59 -0700

    Began basic receive controller and services

---
+++ client/receive/receive.services.js
@@ -0,0 +1,15 @@
+angular.module('bitclip.receive', [
+])
+
+.factory('Address', function() {
+  var address = ;
+
+  var newAddress = function() {
+
+  };
+
+  return {
+  	address: address,
+  	newAddress: newAddress
+  };
+});
