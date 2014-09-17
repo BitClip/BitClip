6254ca441b28115449880e9638e2360e110b9a66
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-16 20:40:22 -0700

    save point

--- client/app.js
+++ client/app.js
@@ -1,3 +1,23 @@
-'use strict';
+angular.module('bitclip', [
+  'ui.router'
+  // 'bitclip.send'
+])
 
-angular.module('bitclip', [])
+.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    // $urlRouterProvider.otherwise('/send');
+
+    $stateProvider
+      .state('send', {
+        url: '/send',
+        templateUrl: 'send/test.html'
+        // controller: 'sendController'
+      });
+  }
+])
+
+.run(['$state',
+  function($state) {
+    $state.transitionTo('send');
+  }
+])
