bdff96a3201d13a1a747fd97b26c31018ad5c400
Etienne Tripier <etripier@gmail.com>
2014-09-17 15:46:36 -0700

    Created browser version of BitcoinJS, v1.0.2

--- client/receive/receive.services.js
+++ client/receive/receive.services.js
@@ -2,10 +2,28 @@ angular.module('bitclip.receive', [
 ])
 
 .factory('Address', function() {
-  var address = ;
+  var address = StorageArea.get('currentAddress', function(address) {
+  	if (!address) {
+  		return newAddress();
+  	}
+  	return address;
+  });
 
   var newAddress = function() {
+  	key = bitcoin.ECKey.makeRandom();
 
+	// Print your private key (in WIF format)
+	key.toWIF();
+
+	// Print your public key (toString defaults to a Bitcoin address)
+	bitcoinAddress = key.pub.getAddress().toString();
+
+  	StorageArea.set({currentAddress: bitcoinAddress}, function(err) {
+  		if (err) {
+  			console.log('Failed to write to Chrome local storage', err);
+  		}
+  		return bitcoinAddress;
+  	});
   };
 
   return {
