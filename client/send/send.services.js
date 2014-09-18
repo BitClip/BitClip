f0a509c9f720fa79e72bfc37025b0b021b592f76
Austin Connelly <ajjconnelly@gmail.com>
2014-09-17 23:19:52 -0700

    test transactions

--- client/send/send.services.js
+++ client/send/send.services.js
@@ -20,9 +20,28 @@ angular.module('bitclip.sendService', [
 
 
   //TODO: sendPayment logic
-
   return {
     transactionDetails: transactionDetails,
     updateTransaction: updateTransaction
   };
+})
+
+.factory('sendTransactionBuilder', function(){
+  
+  var sendTransaction = function(privateKeyWIF, transactionObj){
+    var amount = transactionObj.amount;
+    var destination = transactionObj.destination;
+    console.log("TxObj:", amount, destination)
+    var ecKey = bitcoin.ECKey.fromWIF(privateKeyWIF);
+    var ecKeyAddress = ecKey.pub.getAddress().toString();
+    console.log('ecKey..', ecKey);
+    console.log('ecKeyAddress..', ecKeyAddress);
+  }
+
+
+
+  return {
+    sendTransaction:sendTransaction
+  };
+
 })
