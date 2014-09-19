5c4157aa1dde2043bcff767fcedb6ecb2c05c5e5
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-18 21:28:09 -0700

    Partly implement header factory

--- client/header/header.controller.js
+++ client/header/header.controller.js
@@ -1,16 +1,19 @@
 angular.module('bitclip.header', [])
-// .controller('HeaderController', ['$scope', 'getBalance',
-//   function($scope, getBalance) {
 
-//     chrome.storage.local.get('address').then(function(data) {
-//       getBalance.getBalance(address).then(function(data) {
-//         $scope.balance = data.balance;
-//       })
-//     }).catch(function(err) {
-//       $scope.balance = null;
-//     })
+.controller('HeaderController', ['$scope', 'getBalance',
+  function($scope, getBalance) {
 
-//     $scope.message = ($scope.balance) ? "Your Balance: " + $scope.balance : "No Address Found";
+    chrome.storage.local.get('currentAddress')
+      .then(function(data) {
+        getBalance.getBalance(address).then(function(data) {
+          $scope.balance = data.balance;
+        })
+      })
+      .catch(function(err) {
+        $scope.balance = null;
+      })
 
-//   }
-// ])
+    $scope.message = ($scope.balance) ? "Your Balance: " + $scope.balance : "No Address Found";
+
+  }
+])
--- client/header/header.services.js
+++ client/header/header.services.js
@@ -1,5 +1,32 @@
-// angular.module('bitclip.header', [])
+angular.module('bitclip.headerServices', [])
 
-// .factory('HeaderController', ['$scope' function($scope){
+.factory('getBalance', ['$http',
+  function($http) {
+    //query the helloblock api to get confirmed balance
+    //in all addresses
+    var getBalanceForAllAddresses = function(array) {
+      var addressParam = array.join('&addresses=');
+      var url = 'http://testnet.helloblock.io/v1/addresses?addresses=' + addressParam;
+      console.log("address: ", addressParam);
+      $http({
+        method: 'GET',
+        url: url
+      }).
+      success(function(data, status, headers, config) {
+        console.log('DATA: ', data);
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log('Error: ', data);
+      });
+    };
 
-// }])
+    //query the helloblock api to get confirmed balance
+    //in single address
+    var getBalanceForSingleAddress = function(address) {
+      var url = 'http://testnet.helloblock.io/v1/addresses/' + address;
+
+    };
+  }
+])
