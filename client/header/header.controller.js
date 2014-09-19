dbdd795435e92ef8cf1040accab596590fbf7a98
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-19 00:01:32 -0700

    (feat) Implemented checking of master balance

--- client/header/header.controller.js
+++ client/header/header.controller.js
@@ -1,19 +1,21 @@
 angular.module('bitclip.header', [])
 
-.controller('HeaderController', ['$scope', 'getBalance',
-  function($scope, getBalance) {
+.controller('HeaderController', ['$scope', 'GetBalance',
+  function($scope, GetBalance) {
 
-    chrome.storage.local.get('currentAddress')
-      .then(function(data) {
-        getBalance.getBalance(address).then(function(data) {
-          $scope.balance = data.balance;
-        })
+    GetBalance.getAllAddresses().then(function(addressArray) {
+      GetBalance.getBalanceForAllAddresses(addressArray, true).then(function(data) {
+        var balanceArray = data.data.addresses;
+        var sum = balanceArray.reduce(function(prevValue, currentObj, index) {
+          return prevValue + currentObj.confirmedBalance;
+        }, 0);
+        $scope.balanceMessage = "Balance: " + sum + " BTC";
+      }).catch(function(error) {
+        console.error(error);
       })
-      .catch(function(err) {
-        $scope.balance = null;
-      })
-
-    $scope.message = ($scope.balance) ? "Your Balance: " + $scope.balance : "No Address Found";
-
+    }).catch(function(error) {
+      console.error(error);
+    });
+    $scope.balanceMessage = "Loading Bitcoin address";
   }
 ])
