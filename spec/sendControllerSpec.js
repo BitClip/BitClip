5878771c830a9c393c65abc56f7e26edef6fcefd
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-22 23:35:32 -0700

    Check point - writing helper functions for tests

--- spec/sendControllerSpec.js
+++ spec/sendControllerSpec.js
@@ -2,15 +2,13 @@ describe('sendController', function () {
   // Load the module with MainController
   beforeEach(module('bitclip'));
   var tempStore;
-  var $scope, $rootScope, $location, $window, createController, persistentTransaction, TxBuilder, Utilities;
+  var $scope, $rootScope, $location, $window, createController, TxBuilder, persistentTransaction,Utilities;
 
   beforeEach(inject(function($injector) {
     $rootScope = $injector.get('$rootScope');
     $location = $injector.get('$location');
     $scope = $rootScope.$new();
     $window = $injector.get('$window');
-
-    persistentTransaction = $injector.get('persistentTransaction');
     TxBuilder = $injector.get('TxBuilder');
     Utilities = $injector.get('Utilities');
     console.log("persistenttx", persistentTransaction.transactionDetails);
@@ -60,25 +58,25 @@ describe('sendController', function () {
     //$window.localStorage.removeItem('com.shortly'); //something like this but for chrome storage
   });
 
-  it('tacos', function () {
-    expect(true).to.equal(true);
-  });
+  // it('tacos', function () {
+  //   expect(true).to.equal(true);
+  // });
 
-  it('updateTransaction and sendTransaction should exists', function () {
-    expect(persistentTransaction.updateTransaction).to.be.a('function');
-    expect(TxBuilder.sendTransaction).to.be.a('function');
-  });
+  // it('updateTransaction and sendTransaction should exists', function () {
+  //   expect(persistentTransaction.updateTransaction).to.be.a('function');
+  //   expect(TxBuilder.sendTransaction).to.be.a('function');
+  // });
 
-  xit('updateTransaction should update transactionDetails', function () {
-    var thing = {
-      amount: 'thingOne',
-      destination: 'thingTwo'
-    };
-    console.log(thing);
-    persistentTransaction.updateTransaction(thing);
-    console.log(persistentTransaction.transactionDetails);
-    expect(persistentTransaction.transactionDetails).to.equal("");
-  });
+  // xit('updateTransaction should update transactionDetails', function () {
+  //   var thing = {
+  //     amount: 'thingOne',
+  //     destination: 'thingTwo'
+  //   };
+  //   console.log(thing);
+  //   persistentTransaction.updateTransaction(thing);
+  //   console.log(persistentTransaction.transactionDetails);
+  //   expect(persistentTransaction.transactionDetails).to.equal("");
+  // });
 
   // it('does something else', function () {
   //   expect(true).to.equal(false);
