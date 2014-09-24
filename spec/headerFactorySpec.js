5d4b6c2d468b0fc240475229133d491445d8bf90
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-24 15:16:41 -0700

    (Test) Foundation tests for wallet written, async tests not working - need to fix

--- spec/headerFactorySpec.js
+++ spec/headerFactorySpec.js
@@ -62,21 +62,26 @@ describe('Unit: headerFactory', function () {
     expect(Header.setNetwork).to.be.a('function');
   });
 
-  it('setNetwork change isMainNet in chrome.storage.local', function () {
+  // This async test totally works!!
+  it('setNetwork change isMainNet in chrome.storage.local', function (done) {
     Header.setNetwork(true, function(){
-      console.log("tempStore", tempStore);
       expect(tempStore.isMainNet).to.equal(true);
+      done();
     });
   });
 
-  it('getBalanceForCurrentAddress should return the correct balance for the currentAddress', function () {
+  //this test does not work
+  it('getBalanceForCurrentAddress should return the correct balance for the currentAddress', function (done) {
     Header.getBalanceForCurrentAddress().then(function(currentBalance){
       var currentBalance1 = currentBalance;
       Utilities.httpGet('http://testnet.helloblock.io/v1/addresses/mjjeyn6Vs4TAtMFKJEwpMPJsAVysxL4nYG', function(data){
         var currentBalance2 = data.data.address.balance;
         expect(currentBalance1).to.equal(currentBalance2);
+        done();
       });
-    });
+    }).catch(function(error){
+      done(error);
+    })
   });
 
 })
