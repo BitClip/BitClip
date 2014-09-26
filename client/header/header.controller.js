4badfdfa38d780cad596948ee125a84e7eeda917
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-25 20:03:57 -0700

    (Fix) Fixed bug where updating balance of currentAddress does not work after a new currentAddress is set

--- client/header/header.controller.js
+++ client/header/header.controller.js
@@ -2,6 +2,7 @@ angular.module('bitclip.headerController', [])
 
 .controller('headerController', ['$scope', '$state', 'Header', 'Utilities', function($scope, $state, Header, Utilities) {
   Utilities.initialize().then(function(resolveMessage) {
+
     var setBalance = function() {
       $scope.balanceMessage = 'Loading balance ...';
       Header.getBalanceForCurrentAddress().then(function(balance) {
@@ -12,12 +13,14 @@ angular.module('bitclip.headerController', [])
           console.log("balance: ", $scope.balanceMessage);
         }
       });
+      //open a socket for the current address and
+      //close socket for previous address
       Utilities.getLiveBalanceForCurrentAddress(function(err, data){
         if (err){
           console.error(err);
         } else {
           $scope.balanceMessage = "Bal: " + data.address.balance/100000000 + " BTC";
-        } 
+        };
       });
     };
 
