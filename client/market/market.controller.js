0e826d4acaac7dc94f9a77ef2e6bc5c952d3fffa
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-26 17:50:49 -0700

    Basic scaffolding for markets view complete

--- client/market/market.controller.js
+++ client/market/market.controller.js
@@ -1,14 +1,21 @@
 angular.module('bitclip.marketController', ['nvd3ChartDirectives'])
-.controller('marketController', ['$scope', 'Market', function($scope, Market){
-
+.controller('marketController', ['$scope', 'Market', '$http', function($scope, Market, $http){
     $scope.getGraphData = function(hours){
-    	//first parameter is base url of our server
-    	//do NOT end the url with '/'
-    	console.log(hours);
-    	Market.getGraphData("http://localhost:3000", hours, function(data){
-    		data = JSON.parse(data);
-    		$scope.mktData = data.transactions;
-    	});
+        Market.getGraphData(hours, function(data){
+            console.log(data);
+            if (data !== "Error with HTTP request"){
+                //assign scope variables to populate market view
+                //need to process data.transactions matrix and return 
+                //matrix with local timezone time, and only 2 decimal places
+                $scope.transactions = Market.parseTxIntoTwoDecimals(data.transactions);
+                $scope.updateTime = data.time || 1411777376752;
+                $scope.vwap = data.vwap || '400';
+                $scope.stdDeviation = data.stdDeviation || '15';
+                $scope.max = data.max || '500';
+                $scope.min = data.min || '200';
+                $scope.volume = data.volume || '2000';
+                $scope.lastTrade = Market.getLastTrade(data.transactions);
+            }
+        });
     };
-  }
-]);
+}]);
