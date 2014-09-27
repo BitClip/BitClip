fe1a573502935248c09105cbd0a15ca4c96dab83
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-26 17:50:49 -0700

    Basic scaffolding for markets view complete

--- client/market/market.factory.js
+++ client/market/market.factory.js
@@ -1,20 +1,56 @@
 angular.module('bitclip.marketFactory', [])
-.factory('Market', ['Utilities', function(Utilities){
 
-  var getGraphData = function(baseUrl, numHrs , callback){
-    var url = baseUrl + "/api/marketdata";
+.factory('Market', ['$http', function($http){
+
+  var getGraphData = function(hours, callback){
+    var url = "http://localhost:3000/api/marketdata";
     var dataObj = {
-      timePeriod: numHrs*3600000,
-      time: new Date().getTime() //get current time in milliseconds
+          timePeriod: hours * 3600000,
+          time: new Date().getTime() //get current time in milliseconds
     };
-    var options = JSON.stringify({
-      data:dataObj
+    var options = {
+      data : dataObj,
+      'Content-type': 'application/json'
+    };
+
+    $http.get(url, options).success(function(data){
+      callback(data);
+    }).error(function(data, statusCode){
+      callback("Error with HTTP request");
     });
-    Utilities.httpGet(url, callback, options);
+  };
+
+  var getLastTrade = function(txObj){
+    //first element is time of trade
+    //second element is price of trade
+    var result = [0,0];
+    for (var exchange in txObj){
+      var tradesForExchange = txObj[exchange].values;
+      var finalTrade = tradesForExchange[tradesForExchange.length-1];
+      if (finalTrade[0] > result[0]){
+        result = finalTrade;
+      }
+    };
+    result[1] = +result[1].toFixed(2);
+    //we only need to return trading price, not the time of trade
+    return result[1];
+  };
+
+  //round prices to 2 dec
+  //need to round time to local timezone
+  var parseTxIntoTwoDecimals = function(txObj){
+    for (var exchange in txObj){
+      var tradesForExchange = txObj[exchange].values;
+      tradesForExchange.forEach(function(tradeArr, index, collection){
+        tradeArr[1] = +tradeArr[1].toFixed(2);
+      });
+    }
+    return txObj;
   };
 
   return {
-    getGraphData:getGraphData
+    getGraphData:getGraphData,
+    getLastTrade: getLastTrade,
+    parseTxIntoTwoDecimals: parseTxIntoTwoDecimals
   };
-
 }])
