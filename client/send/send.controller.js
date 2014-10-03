ebb72295d7a8a06188bd8f1e71f388db9073ca16
JackTanRoo <ordo_sagittae@hotmail.com>
2014-10-03 11:20:50 -0700

    Took out console logs in send

--- client/send/send.controller.js
+++ client/send/send.controller.js
@@ -1,56 +1,49 @@
 angular.module('bitclip.sendController', [
   'ngFx'
-  ])
+])
 
 .controller('sendController', ['$scope', '$timeout', 'TxBuilder','Utilities',
-  function($scope, $timeout, TxBuilder, Utilities) {
-    
-  var displayError = function() {
-    if ($scope.sendForm.destination.$invalid && $scope.sendForm.amount.$invalid) {
-      $scope.notification = 'Invalid Destination and Transaction Amount';
-    } else if ($scope.sendForm.destination.$invalid) {
-      $scope.notification = 'Invalid Destination';
-    } else if ($scope.sendForm.amount.$invalid) {
-      $scope.notification = 'Invalid Transaction Amount';
-    }
-    if($scope.notification) {
-      $timeout(function() { 
-        $scope.notification = false;
-      }, 2000);
-    } else {
-      $scope.morph();
-    }
-  };
+  function($scope, $timeout, TxBuilder, Utilities) {  
+    var displayError = function(){
+      if ($scope.sendForm.destination.$invalid && $scope.sendForm.amount.$invalid){
+        $scope.notification = 'Invalid Destination and Transaction Amount';
+      } else if ($scope.sendForm.destination.$invalid){
+        $scope.notification = 'Invalid Destination';
+      } else if ($scope.sendForm.amount.$invalid){
+        $scope.notification = 'Invalid Transaction Amount';
+      }
+      if(!$scope.notification) $scope.morph(); 
+    };
 
-  $scope.transactionDetails = {};
+    $scope.transactionDetails = {};
 
-  Utilities.isMainNet().then(function(isMainNet) {
-    $scope.network = isMainNet;
-  });
-
-  $scope.morph = function() {
-    $scope.confirmed = !$scope.confirmed;
-  };
-
-  $scope.validateInput = function() {
-    TxBuilder.updateTx($scope.transactionDetails);
-    $scope.transactionDetails = TxBuilder.getTransactionDetails();
-    displayError();
-  };
-
-  $scope.sendPayment = function() {
-    var updatedTxDetails = TxBuilder.getTransactionDetails();
-    Utilities.getCurrentPrivateKey().then(function(currentPrivateKey) {
-      TxBuilder.sendTransaction(currentPrivateKey, updatedTxDetails, $scope.network)
-      .then(function(message) {
-        $scope.notification = message;
-        $timeout(function() { $scope.notification = false }, 2000);
-      })
-      .catch(function(err) {
-        $scope.notification = "Transaction Failed: "+ err.message;
-        $timeout(function() { $scope.notification = false }, 2000);
-      });
+    Utilities.isMainNet().then(function(isMainNet) {
+      $scope.network = isMainNet;
     });
-    $scope.morph();
-  };
+
+    $scope.morph = function() {
+      $scope.confirmed = !$scope.confirmed;
+    };
+
+    $scope.validateInput = function() {
+      TxBuilder.updateTx($scope.transactionDetails);
+      $scope.transactionDetails = TxBuilder.getTransactionDetails();
+      displayError();
+    };
+
+    $scope.sendPayment = function() {
+      var updatedTxDetails = TxBuilder.getTransactionDetails();
+      Utilities.getCurrentPrivateKey().then(function(currentPrivateKey) {
+        TxBuilder.sendTransaction(currentPrivateKey, updatedTxDetails, $scope.network)
+        .then(function(message) {
+          $scope.notification = message;
+          $timeout(function() { $scope.notification = false }, 2000);
+        })
+        .catch(function(err) {
+          $scope.notification = "Transaction Failed: "+ err.message;
+          $timeout(function() { $scope.notification = false }, 2000);
+        });
+      });
+      $scope.morph();
+    };
 }]);
