eeb118014e32fe445ee1ed539b3c45ebdd77f88d
JackTanRoo <ordo_sagittae@hotmail.com>
2014-10-06 16:01:31 -0700

    (feat) Market spinner done

--- client/market/market.controller.js
+++ client/market/market.controller.js
@@ -2,6 +2,7 @@ angular.module('bitclip.marketController', ['nvd3ChartDirectives'])
 
 .controller('marketController', ['$scope', 'Market', '$http', function($scope, Market, $http){
   $scope.getGraphData = function(hours) {
+    $scope.loading =true;
     Market.getGraphData(hours, function(data) {
       $scope.setYAxis = [Math.ceil((+data.min * 0.98)), Math.ceil((+data.max * 1.02))];
       $scope.transactions = Market.parseTxIntoTwoDecimals(data.transactions);
@@ -12,16 +13,16 @@ angular.module('bitclip.marketController', ['nvd3ChartDirectives'])
       $scope.min = +data.min.toFixed(2);
       $scope.volume = +data.volume.toFixed(2);
       $scope.lastTrade = Market.getLastTrade(data.transactions);
+      $scope.loading = false;
     });
   };
 
   $scope.toolTipContentFunction = function() {
-
     return function(exchangeName, date, price, e, graph) {
       exchangeName = exchangeName.charAt(0).toUpperCase() + exchangeName.slice(1);
       var template = "<div class='graphToolTip'><small class='toolTipHeader'>" + exchangeName + 
-        "</small><small class='toolTipPrice'><span>" + price + "</span></small>" + 
-        "<small class='toolTipTime'>" + date + "</small></div>";
+        "</small><small class='toolTipPrice'><span>$" + price + "</span></small>" + 
+        "<small class='toolTipTime'>@" + date + "</small></div>";
       return template;
     };
   };
@@ -29,16 +30,8 @@ angular.module('bitclip.marketController', ['nvd3ChartDirectives'])
   $scope.xAxisTickValuesFunction = function() {
     return function(d) {
       var tickVals = [];
-      var values = d[0].values;
-      var valLength = values.length;
-
-      if(valLength < 2) { //default if server hasn't collected enough data
-        return [0, 0];
-      }
-
-      var mid = Math.ceil(valLength/2)
-      tickVals.push(values[mid][0])
-
+      var mid = Math.ceil(d[0].values.length/2)
+      tickVals.push(d[0].values[mid][0])
       return tickVals;
     };
   };
